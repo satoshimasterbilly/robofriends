9ea893fd16ab0e02b34429703e372ddbd1c321c7
satoshimasterbilly <billy.mcdavid2@yahoo.com>
2022-03-02 11:25:24 +0000

    made change to return -cancel this

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = (props) => {
-    
+
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
            <img alt='robots' src={'https://robohash.org/${props.id}test?200x200'} />
